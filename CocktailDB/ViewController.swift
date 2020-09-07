7cc19a3aa00f9c786720e06e6a59da46f132d5e1
Ronfern <rofnffern@gmail.com>
2020-09-07 12:21:21 +0300

    Initial Commit

---
+++ CocktailDB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CocktailDB
+//
+//  Created by Роман Чугай on 07.09.2020.
+//  Copyright © 2020 Роман Чугай. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
